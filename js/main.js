fb56b5c499c6ade8d7fdb0e50ae2985d1e43971b
TastyCooler <34125083+TastyCooler@users.noreply.github.com>
2021-03-29 18:53:43 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var h_text = new Array();
+
+h_text[0]= "Be happy with what you have. Be excited about what you want by Alan Cohen." 
+h_text[1]="If you look to others for fulfillment, you will never be fulfilled. If your happiness depends on money, you will never be happy with yourself. Be content with what you have; rejoice in the way things are. When you realize there is nothing lacking, the world belongs to you by Lao Tzu." 
+h_text[2]="True happiness is not attained through self-gratification, but through fidelity to a worthy purpose by Helen Keller." 
+h_text[3]="When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us by Helen Keller." 
+h_text[4]="There is a powerful driving force inside every human being that, once unleashed, can make any vision, dream, or desire a reality. -Tony Robbins"
+h_text[5]="The Real joy in life comes from finding your true purpose and aligning it with what you do every single day. -Tony Robbins"
+h_text[6]="Always dream and shoot higher than you know you can do. Don’t bother just to be better than your contemporaries or predecessors. Try to be better than yourself. —William Faulkner"
+h_text[7]="Mediocrity will never do. You are capable of something better. —Gordon B. Hinckley"
+h_text[8]="The best and most beautiful things in the world cannot be seen or even touched. They must be felt with the heart.Helen Keller"
+h_text[9]="Life is 10% what happens to me and 90% of how I react to it.-Charles Swindoll"
+h_text[10]="Don’t judge each day by the harvest you reap but by the seeds that you plant.-Robert Louis Stevenson"
+
+
+var i = Math.floor(h_text.length * Math.random());
+document.write( h_text[i]);
